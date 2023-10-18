f698c524102c9af6ba6405bbcd79f890d351afb4
DuckChis <rjsdn1862@daum.net>
2023-10-18 09:23:29 +0900

    fix : untracked files

--- src/main/java/com/example/projectfinal/repository/Predict_RelationshipRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectfinal.repository;
-
-import com.example.projectfinal.entity.function.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Predict_RelationshipRepo extends JpaRepository<Orders, String> {
-}
